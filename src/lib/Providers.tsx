4d643ebb0fb7f48eef3080fa05fb6f8cc57c2ac8
asifikbal09 <asif.ikbal662@gmail.com>
2025-12-15 16:37:30 +0600

    feat: integrate Redux Toolkit and set up store; update Providers for Redux provider

--- src/lib/Providers.tsx
+++ src/lib/Providers.tsx
@@ -1,11 +1,15 @@
 "use client";
 import { Toaster } from "sonner";
 import { AuthProvider } from "./AuthProviders";
+import { Provider } from "react-redux";
+import { store } from "@/redux/store";
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <AuthProvider>
-      <Toaster></Toaster>
-      {children}
+      <Provider store={store}>
+        <Toaster></Toaster>
+        {children}
+      </Provider>
     </AuthProvider>
   );
 }
